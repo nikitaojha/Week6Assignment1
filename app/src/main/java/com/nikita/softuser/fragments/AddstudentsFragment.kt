5dcae48f9fa8220ea9096d131ae0312f5c751b7e
nikitaojha <nikiojha2018@gmail.com>
2021-01-13 14:06:39 +0545

    Assignment

--- app/src/main/java/com/nikita/softuser/fragments/AddstudentsFragment.kt
+++ app/src/main/java/com/nikita/softuser/fragments/AddstudentsFragment.kt
@@ -5,31 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
+import android.widget.TextView
 import com.nikita.softuser.R
 
 class AddstudentsFragment : Fragment() {
     private lateinit var etfullname: EditText
     private lateinit var etage: EditText
     private lateinit var etaddress: EditText
-    private lateinit var radioMale : RadioButton
-    private lateinit var radioFemale : RadioButton
-    private lateinit var radioothers : RadioButton
+    private lateinit var tvgender: TextView
+    private lateinit var radioMale: RadioButton
+    private lateinit var radioFemale: RadioButton
+    private lateinit var radioothers: RadioButton
+    private lateinit var btnsave: Button
 
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_addstudents, container, false)
+        val view = inflater.inflate(R.layout.fragment_addstudents, container, false)
+        etfullname = view.findViewById(R.id.etfullname)
+        etage = view.findViewById(R.id.etage)
+        etaddress = view.findViewById(R.id.etaddress)
+        tvgender = view.findViewById(R.id.tvgender)
+        radioMale = view.findViewById(R.id.radioMale)
+        radioFemale = view.findViewById(R.id.radioFemale)
+        radioothers = view.findViewById(R.id.radioOthers)
+        btnsave = view.findViewById(R.id.btnsave)
+
+
+
     }
 }
+
 
